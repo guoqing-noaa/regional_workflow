5dc02799aec2877e44f59ad6eefb8d0b08d13955
guoqing.ge <Guoqing.Ge@noaa.gov>
2021-09-16 23:26:58 -0600

    RRFS_FIX --> FIX_RRFS

--- ush/fix_rrfs_locations.sh
+++ ush/fix_rrfs_locations.sh
@@ -4,32 +4,32 @@
 #
 if [[ -d /dcom && -d /hwrf ]] ; then
     PLATFORM=wcoss
-    RRFS_FIX_LOCATION="/to/do"
+    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /cm ]] ; then
     PLATFORM=wcoss_c
-    RRFS_FIX_LOCATION="/to/do"
+    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /ioddev_dell ]]; then
     PLATFORM=wcoss_d
-    RRFS_FIX_LOCATION="/to/do"
+    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /scratch1 ]] ; then
     PLATFORM=hera
-    RRFS_FIX_LOCATION="/scratch2/BMC/rtrr/FIX_RRFS"
+    FIX_RRFS_LOCATION="/scratch2/BMC/rtrr/FIX_RRFS"
 elif [[ -d /carddata ]] ; then
     PLATFORM=s4
-    RRFS_FIX_LOCATION="/to/do"
+    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /jetmon ]] ; then
     PLATFORM=jet
-    RRFS_FIX_LOCATION="/lfs4/BMC/nrtrr/FIX_RRFS"
+    FIX_RRFS_LOCATION="/lfs4/BMC/nrtrr/FIX_RRFS"
 elif [[ -d /glade ]] ; then
     PLATFORM=cheyenne
-    RRFS_FIX_LOCATION="/to/do"
+    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /sw/gaea ]] ; then
     PLATFORM=gaea
-    RRFS_FIX_LOCATION="/to/do"
+    FIX_RRFS_LOCATION="/to/do"
 elif [[ -d /work ]]; then
     PLATFORM=orion
-    RRFS_FIX_LOCATION="/work/noaa/rtrr/FIX_RRFS"
+    FIX_RRFS_LOCATION="/work/noaa/rtrr/FIX_RRFS"
 else
     PLATFORM=unknow
-    RRFS_FIX_LOCATION="/this/is/an/unknow/platform"
+    FIX_RRFS_LOCATION="/this/is/an/unknow/platform"
 fi
